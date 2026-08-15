76213cf5e6c523460f77882de0e204affb17d2bb
Luv Tomar <luvtomar2005@gmail.com>
2026-08-15 16:42:59 +0530

    Time: 52 ms (84.27%), Space: 68.6 MB (87.64%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,30 +1,21 @@
-// this is basically the count substring with at most k distinct characters the solution before it was brute force and now it is sliding window
-
-
+/* The logic is simple as the same logic as maximum length of substring with k distinct character..  */
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0;
-        int maxLen = 0;
-        
-        for (int right = 0; right < fruits.length; right++) {
-            // 1. Add the current fruit from the right pointer to the map
-            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+        int left =0;
+        int maxFruits = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int right = 0;right < fruits.length;right++) {
+            map.put(fruits[right] , map.getOrDefault(fruits[right] , 0)  + 1);
 
-            // 2. If we have more than 2 types of fruits, shrink the window from the left
-            while (map.size() > 2) {
-                map.put(fruits[left], map.get(fruits[left]) - 1);
-
-                if (map.get(fruits[left]) == 0) {
+            if(map.size()  > 2){
+                map.put(fruits[left] , map.get(fruits[left]) - 1);
+                if(map.get(fruits[left]) == 0) {
                     map.remove(fruits[left]);
                 }
-                left++; // Move the left pointer forward
+                left++;
             }
-            
-            // 3. Update the maximum length of a valid window
-            maxLen = Math.max(maxLen, right - left + 1);
+            maxFruits = Math.max(maxFruits , right - left + 1);
         }
-        
-        return maxLen;
+        return maxFruits;
     }
 }
