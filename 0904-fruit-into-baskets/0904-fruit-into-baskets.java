6b4d6fa6d066ccb6ccefbab396a49d9d8a8ca4d0
Luv Tomar <luvtomar2005@gmail.com>
2026-08-18 00:29:25 +0530

    Time: 54 ms (66.57%), Space: 70.4 MB (69.36%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,20 +1,20 @@
 /* The logic is simple as the same logic as maximum length of substring with k distinct character..  */
-class Solution {
-    public int totalFruit(int[] fruits) {
-        int left =0;
+class Solution{
+    public int totalFruit(int [] nums){
+        int left = 0;
         int maxFruits = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int right = 0;right < fruits.length;right++) {
-            map.put(fruits[right] , map.getOrDefault(fruits[right] , 0)  + 1);
-
-            if(map.size()  > 2){
-                map.put(fruits[left] , map.get(fruits[left]) - 1);
-                if(map.get(fruits[left]) == 0) {
-                    map.remove(fruits[left]);
+        for(int right = 0;right < nums.length;right++){
+            map.put(nums[right] , map.getOrDefault(nums[right] , 0) + 1);
+            while(map.size() > 2){
+                map.put(nums[left] , map.get(nums[left]) - 1);
+                if(map.get(nums[left]) == 0){
+                    map.remove(nums[left]);
+                    
                 }
                 left++;
             }
-            maxFruits = Math.max(maxFruits , right - left + 1);
+            maxFruits = Math.max(maxFruits, right - left + 1);
         }
         return maxFruits;
     }
