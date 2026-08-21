ab71b6b573c0e060667c2e74fe09a33d0b7e35df
Luv Tomar <luvtomar2005@gmail.com>
2026-08-21 22:19:05 +0530

    Time: 6 ms (87.75%), Space: 44 MB (60.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,11 +1,11 @@
-class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        int[] pCount = new int[26];
+class Solution{
+    public boolean checkInclusion(String s1 , String s2){
+        int [] pCount = new int[26];
         int [] sCount = new int[26];
         for(char c : s1.toCharArray()){
             pCount[c - 'a']++;
         }
-        for(int i =0;i<s2.length();i++){
+        for(int i = 0;i<s2.length();i++){
             sCount[s2.charAt(i) - 'a']++;
             if(i >= s1.length()){
                 sCount[s2.charAt(i - s1.length()) - 'a']--;
