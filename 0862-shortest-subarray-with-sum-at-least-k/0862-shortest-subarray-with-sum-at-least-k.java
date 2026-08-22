c832c07c4f72b0e658fe2888790812f4a9c3d776
Luv Tomar <luvtomar2005@gmail.com>
2026-08-22 13:13:41 +0530

    Time: 30 ms (20.7%), Space: 68.2 MB (19.48%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
@@ -1,35 +1,37 @@
-class Solution {
-    // Solving using Prefix sum + deque pattern -> codewithmik logic
-    public int shortestSubarray(int[] nums, int k) {
+class Solution{
+    public int shortestSubarray(int [] nums , int k) {
         int n = nums.length;
-        Deque<Integer> deq =new LinkedList<>();
+        Deque<Integer> deq = new LinkedList<>();
         long [] prefixSum = new long[n];
         int result = Integer.MAX_VALUE;
         int j = 0;
         while(j < n){
             if(j == 0){
-                prefixSum[j] = nums[j]; 
+                prefixSum[j] = nums[j];
             }
             else{
                 prefixSum[j] = prefixSum[j - 1] + nums[j];
-
             }
             if(prefixSum[j] >= k){
                 result = Math.min(result , j + 1);
             }
             while(!deq.isEmpty() && prefixSum[j] - prefixSum[deq.peekFirst()] >= k){
-                result = Math.min(result , j - deq.peekFirst()); // calculating the length of the subarray
-                deq.pollFirst(); // remove the front index
+                result = Math.min(result , j - deq.peekFirst());
+                deq.pollFirst();
             }
-            //  Maintaining the montoic property of deque
             while(!deq.isEmpty() && prefixSum[j] <= prefixSum[deq.peekLast()]){
                 deq.pollLast();
             }
-            // Add the current index
+            // Adding the current index
             deq.offerLast(j);
             j++;
         }
         return result == Integer.MAX_VALUE ? -1 : result;
     }
 }
+
+//         }
+//         return result == Integer.MAX_VALUE ? -1 : result;
+//     }
+// }
 
