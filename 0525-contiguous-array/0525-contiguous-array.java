5627e6044c6abc3125398de6a28d6a3ea98de764
Luv Tomar <luvtomar2005@gmail.com>
2026-08-25 21:13:26 +0530

    Time: 25 ms (19.97%), Space: 65.6 MB (42.17%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -2,26 +2,22 @@ class Solution{
     public int findMaxLength(int [] nums){
         HashMap<Integer,Integer> map = new HashMap<>();
         map.put(0 , -1);
-        int sum = 0;
-        int maxLen = 0;
+        int prefixSum = 0;
+        int maxLength = 0;
         for(int i = 0;i<nums.length;i++){
-            // convert 0 to -1
             if(nums[i] == 0){
-                sum += -1;
+                prefixSum += -1;
             }
             else{
-                sum += 1;
+                prefixSum += 1;
             }
-            if(map.containsKey(sum)){
-                maxLen = Math.max(maxLen, i - map.get(sum));
-
+            if(map.containsKey(prefixSum)){
+                maxLength = Math.max(maxLength , i - map.get(prefixSum));
             }
             else{
-                map.put(sum , i);
+                map.put(prefixSum , i);
             }
-           
         }
-         return maxLen;
-    }   
+        return maxLength;
+    }
 }
-
